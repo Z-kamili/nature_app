c2c032f09e5f8e775fe87d81fa6a8bdb3b274fa4
Z-kamili <zakariakamili97@gmail.com>
2022-10-10 15:15:07 +0100

    add bar search in main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
 
         <main class="container mx-auto max-w-custom flex" style="max-width:1000px">
             <div class="w-70 mr-5">
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod vitae sapiente optio hic rem voluptate doloribus, facere quisquam fugiat ex modi quas. Harum iste culpa mollitia quis consequatur dolor rerum, ullam temporibus illo dignissimos eaque ut quibusdam maxime totam eius fugiat commodi nam animi cumque labore quaerat quae est. Deleniti voluptas quasi facilis nisi quam cupiditate ad itaque ipsam quos sunt tempore impedit architecto quis minus repellendus, distinctio provident assumenda sapiente sit possimus cumque. Numquam debitis animi ex quaerat similique reiciendis quia eos dolor culpa assumenda temporibus, dignissimos, necessitatibus totam iste obcaecati quis, labore molestias perferendis illum. Eligendi voluptatibus suscipit beatae, distinctio necessitatibus vero blanditiis nisi quis quam in ut eum. Veritatis reiciendis corrupti consequuntur alias ipsam explicabo maiores aperiam repellat quasi tempora? Assumenda cumque adipisci voluptatum perferendis cupiditate et dolorum incidunt? Rem et excepturi natus. Earum totam recusandae nesciunt pariatur sapiente? Maiores aperiam minima doloremque expedita harum nulla blanditiis placeat quia perspiciatis sequi, adipisci iste, natus, consequuntur ipsum optio ratione. Dicta aliquam totam enim pariatur quis consequatur accusantium rem debitis, libero dolor ab natus necessitatibus magni repellat quas veniam modi fuga repellendus quae! Numquam eaque asperiores beatae libero sequi facere consequatur distinctio sunt obcaecati. Reprehenderit odit voluptate perferendis.
+               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur autem iusto, natus quo laboriosam, recusandae debitis fuga eveniet velit aliquid ducimus possimus ipsa inventore voluptatum facilis atque aliquam nulla ipsum harum soluta obcaecati eaque. Provident magni architecto cupiditate ducimus quaerat eaque nesciunt adipisci rem eius, magnam sit nam exercitationem ratione molestiae! Aut repudiandae tenetur maxime provident nostrum et obcaecati ipsum consectetur rerum quod ex error corporis debitis, ducimus suscipit temporibus fuga inventore voluptatibus a pariatur soluta tempore asperiores consequuntur nulla? Beatae alias tempora magnam corporis rem ratione soluta aut amet id, tempore dolor vel ducimus tenetur nemo iusto, officiis ullam!
             </div>
             <div class="w-175">
                <nav class="flex items-center justify-between text-xs">
@@ -59,13 +59,19 @@
                         <li><a href="#" class="border-b-4 pb-3 border-blue"> All Idea (87) </a></li>
                         <li><a href="#" class="text-gray-200 transition duration-150 border-b-4 pb-3 hover:border-blue">Considering (6) </a></li>
                         <li><a href="#" class="text-gray-200 transition duration-150 border-b-4 pb-3 hover:border-blue">IN PROGRESS (1) </a></li>
-                        {{-- <li><a href="#" class="">All Idea (87) </a></li> --}}
                     </ul>
                     <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
                         <li><a href="#" class="text-gray-200 transition duration-150 border-b-4 pb-3 hover:border-blue">Implemented (10) </a></li>
                         <li><a href="#" class="text-gray-200 transition duration-150 border-b-4 pb-3 hover:border-blue">Closed (55) </a></li>
                     </ul>
                </nav>
+
+               <div class="mt-8">
+
+                      {{ $slot }}
+
+               </div>
+
             </div>
         </main>
 
