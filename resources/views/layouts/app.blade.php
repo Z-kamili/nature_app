fe7ff3b79250c35ece9cd29847e849ad90ab9c3a
Z-kamili <zakariakamili97@gmail.com>
2022-10-10 11:11:06 +0100

    create header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,33 @@
        
         <header class="flex items-center justify-between px-8 py-4">
 
-            <a href="">?ature Logo</a>
+            <a href=""><img src="{{asset('img/logo.svg')}}" alt="logo"  ></a>
+            <div class="flex items-center">
+                @if (Route::has('login'))
+                <div class="px-6 py-4">
+                    @auth
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
 
+                        <a href="{{route('logout')}}"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </a>
+                    </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+                @endif
+                <a href="#">
+                    <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full main-img">
+                </a>
+            </div>
         </header>
 
     </body>
